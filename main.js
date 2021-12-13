3128df948830649dc4c1599c6286330e4653ffbe
mayankjha3 <84058462+mayankjha3@users.noreply.github.com>
2021-12-13 15:42:25 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 song = "";
 
+leftWristX = 0;
+leftWristY = 0;
+
+rightWristX = 0;
+rightWristY = 0;
+
+
+
 function preload(){
     song = loadSound('music.mp3');
 }
@@ -9,6 +17,10 @@ function setup(){
     canvas.center();
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video , modelLoaded);
+    poseNet.on('pose' , gotPoses);
+
 }
 
 function draw(){
@@ -17,4 +29,27 @@ function draw(){
 
 function play(){
     song.play();
+    song.setVolume(1);
+    song.rate(1);
 }
+
+function modelLoaded(){
+    console.log("model has loaded");
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+
+        console.log("left wrist X " + leftWristX + " left wrist y " + leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+
+        console.log("right wrist X " + rightWristX + " right wrist y " + rightWristY);
+    }
+}
+
